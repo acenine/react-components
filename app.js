32ad2eb40f9a9ffab4ca96ab14fa5f0cd4481320
acenine <acenine@gmx.com>
2017-12-13 15:09:20 -0800

    Complete react components

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-// TODO
+
+
+
+
+
+
+
+
+
+
+
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item => <GroceryListItem item = {item} />
+     )}
+  </ul>
+
+);
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style = {style} onMouseOver = {this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)}> {this.props.item}</li>
+      )
+  }
+
+};
+// var Eggs = () => (
+//   <li>eggs</li>
+// );
+// var Bacon = () => (
+//   <li>bacon</li>
+// );
+ReactDOM.render(<GroceryList items = {['hello', "hey"]}/>, document.getElementById("app"))
